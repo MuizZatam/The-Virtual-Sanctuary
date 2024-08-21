508c4e463bf00bbc39d43053c662e7b36a165e72
MuizZatam <muizzatam110@gmail.com>
2024-08-21 13:00:13 +0530

    Update Application structure

--- App/main.py
+++ App/main.py
@@ -1,16 +1,13 @@
 # Defining Imports
-from Modules.voice_tts.tts import tts
+from Modules.tts import tts
+from Modules.eco_translate import to_species
+from Modules.bing_images import fetch_bing_images
+from Modules.geocode import geocode
 
 
 def main() -> None:
 
-    tts("""
-
-        Welcome to the Virtual Sanctuary!
-
-        I am Ryan!
-        The Narrator of The Virtual Sanctuary!
-    """)
+    ...
 
 
 if __name__ == "__main__":
