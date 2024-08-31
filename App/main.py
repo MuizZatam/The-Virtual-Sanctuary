a41f2938f317792ad53d9938bd54586a6af56863
MuizZatam <muizzatam110@gmail.com>
2024-08-31 12:43:07 +0530

    Update LLM Interface using llama_cpp

--- App/main.py
+++ App/main.py
@@ -2,11 +2,11 @@
 from Modules.tts import tts
 from Modules.bing_images import fetch_bing_images
 from Modules.geocode import geocode
-
+from Modules.responses import narrate
 
 def main() -> None:
 
-    ...
+    print(narrate("Saraca Asoca", "Western Ghats"))
 
 
 if __name__ == "__main__":
