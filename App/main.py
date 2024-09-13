a1b6d01179f46f648388b9ee63455531a6f90f78
MuizZatam <muizzatam110@gmail.com>
2024-09-13 15:35:49 +0530

    Implement backend connectivity

--- App/main.py
+++ App/main.py
@@ -1,51 +1,43 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from Modules.api import API_response
 from Modules.gemini import narrate
 from Modules.coords import coords
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])  
 def index():
-
     if request.method == "POST":
-
-        location = request.form.get("location")
+        location = request.json.get('location')
         
         if not location:
-
             return jsonify({"undocumented-location": "Unfortunately, we don't have that location documented :("})
         
         try:
-
             map_plots = coords(location)
             species = API_response(location)
 
-            specie_list = list()
-            narrations = list()
-
-            for key in species.keys():
-
-                specie_list.append(key)
-            
+            specie_list = list(species.keys())
             narrations = narrate(specie_list, location).split('sep')
 
-            species_data = dict()
-            for index, specie in enumerate(specie_list):
-                
-                species_data[specie] = {
-
+            species_data = {
+                specie: {
                     "narration": narrations[index],
                     "images": species[specie],
                     "coords": map_plots
-                }
+                } for index, specie in enumerate(specie_list)
+            }
 
-            return  jsonify({'species_data': species_data})
+            return jsonify(species_data)
         
         except Exception as e:
-            # Add general error handling
-            return jsonify({'error':'An error occurred: str(e)'})
+            return jsonify({'error': f'An error occurred: {str(e)}'})
     
-
+    else:
+        return ""
+    
 if __name__ == "__main__":
     app.run(debug=True)
